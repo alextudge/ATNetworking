f3123ab201567ab21e43309c0b159b12e525311e
Alex Tudge <alextudge@hotmail.co.uk>
2023-07-26 22:08:42 +0100

    iOS support

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "ATNetworking",
     platforms: [
         .macOS(.v12),
+        .iOS(v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
