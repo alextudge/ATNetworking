fb2dcad2b15e3aaad76ede08aff3ef53d0fa79b7
Alex Tudge <alextudge@hotmail.co.uk>
2023-07-27 09:42:16 +0100

    Access controls for ATNetworkingInitialiser

--- Sources/ATNetworking/Public/ATNetworkingInitialiser.swift
+++ Sources/ATNetworking/Public/ATNetworkingInitialiser.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 public class ATNetworkingInitialiser {
+    
+    public init() {}
+    
     public func generateNetworkService() -> ATNetworkServiceProtocol {
         let networkService = ATNetworkService(session: URLSession.shared)
         return networkService
