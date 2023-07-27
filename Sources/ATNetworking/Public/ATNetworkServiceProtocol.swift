20b1c2abb9173d282f0c64a9ecf41f65c60ddbf2
Alex Tudge <alextudge@hotmail.co.uk>
2023-07-27 10:21:19 +0100

    Document ATNetworkServiceProtocol

--- Sources/ATNetworking/Public/ATNetworkServiceProtocol.swift
+++ Sources/ATNetworking/Public/ATNetworkServiceProtocol.swift
@@ -9,7 +9,33 @@ import Foundation
 import Combine
 
 public protocol ATNetworkServiceProtocol: AnyObject {
+    /**
+     Make a network call with a completion block.
+
+     - Parameter endpoint: A case from an enum conforming to `ATEndpoint`.
+     - Parameter type: A `Decodable` type to decode a successful response into.
+     - Parameter completion: An escaping closure of type `Result<T, ATError>`
+     */
     func request<T: Decodable>(endpoint: ATEndpoint, type: T.Type, completion: @escaping (Result<T, ATError>) -> Void)
+    
+    /**
+     Make a network call with a `Combine` publisher.
+
+     - Parameter endpoint: A case from an enum conforming to `ATEndpoint`.
+     - Parameter type: A `Decodable` type to decode a successful response into.
+     - Returns: A publisher containing the decoded response or any errors.
+     */
     func request<T: Decodable>(endpoint: ATEndpoint, type: T.Type) -> AnyPublisher<T, ATError>
+    
+    /**
+     Make a network call with aync.
+
+     - Parameter endpoint: A case from an enum conforming to `ATEndpoint`.
+     - Parameter type: A `Decodable` type to decode a successful response into.
+     
+     - Throws: An `ATError` case representing the part of the operation that failed (networking, decoding etc).
+     
+     - Returns: The decoded object from a successful response.
+     */
     func request<T: Decodable>(endpoint: ATEndpoint, type: T.Type) async throws -> T
 }
