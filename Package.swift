19870933a278ff7daa398a6055c2ebb7097bcd6c
Alex Tudge <26205302+alextudge@users.noreply.github.com>
2023-08-10 20:51:05 +0100

    Expanding support

--- Package.swift
+++ Package.swift
@@ -7,7 +7,9 @@ let package = Package(
     name: "ATNetworking",
     platforms: [
         .macOS(.v12),
-        .iOS(.v15)
+        .iOS(.v15),
+        .watchOS(.v8),
+        .tvOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
